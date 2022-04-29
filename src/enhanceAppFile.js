f65cb3a7c29d5b2dfdcfffb8e72416d8bf06bfa8
smori <shinichiro.mori.1983@gmail.com>
2022-04-29 11:18:26 +0900

    Initial implement of GitgraphMinigramEditor.vue

--- src/enhanceAppFile.js
+++ src/enhanceAppFile.js
@@ -1,5 +1,7 @@
 import GitgraphMinigramDefault from './components/GitgraphMinigramDefault';
+import GitgraphMinigramEditor from './components/GitgraphMinigramEditor';
 
 export default ({Vue}) => {
   Vue.component('PluginGitgraphMinigramDefault', GitgraphMinigramDefault);
+  Vue.component('PluginGitgraphMinigramEditor', GitgraphMinigramEditor);
 };
